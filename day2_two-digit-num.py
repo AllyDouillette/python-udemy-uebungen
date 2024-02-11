ab8ff554e87c516721e7d86046fe6b8addcd4412
Rebecca Noy <rebecca.noy@gmx.de>
2024-02-11 16:36:34 +0100

    zeug dingens

--- day2_two-digit-num.py
+++ day2_two-digit-num.py
@@ -1,3 +1,6 @@
 two_digit_number = 39
-result = int(str(two_digit_number)[0]) + int(str(two_digit_number)[1])
+two_digit_number = str(two_digit_number)
+first_digit = int(two_digit_number[0])
+second_digit = int(two_digit_number[1])
+result = first_digit + second_digit
 print(result)
