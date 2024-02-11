97a43382e94e885bfe180203da2516314c5a6226
Rebecca Noy <rebecca.noy@gmx.de>
2024-02-11 12:39:51 +0100

    lesson 2

---
+++ lesson2.py
@@ -0,0 +1,6 @@
+name = input("What is your name?")
+print("Hello, good day, " + name + "!")
+birthyear = input("In which year were you born?")
+print(type(birthyear))
+print("You must be around deeznutz years old.")
+
