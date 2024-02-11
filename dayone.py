999721109ed7d6132dfedab9c949b58928d114e0
Rebecca Noy <rebecca.noy@gmx.de>
2024-02-11 15:31:58 +0100

    day one band name generator

---
+++ dayone.py
@@ -0,0 +1,4 @@
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the name of your first pet?\n")
+bandname = city + " " + pet
+print(f"To honour your birthplace of {city} and your first pet being named {pet}, your band name could be:\n{bandname}")
