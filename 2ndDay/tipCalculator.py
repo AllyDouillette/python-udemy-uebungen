aeef734e0a739c52bd2424c8900c3c9275aa7b51
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-02-18 13:09:54 +0100

    sum change

--- 2ndDay/tipCalculator.py
+++ 2ndDay/tipCalculator.py
@@ -1,13 +1,13 @@
 print("Welcome to the tip calculator.")
-totalAmount = input("What was the total bill?")
+totalAmount = input("What was the total bill?\n$")
 totalAmount = float(totalAmount)
 # totalAmount = 124.56
 
-tipPercentage = input("What percentage tip would you like to give?")
+tipPercentage = input("What percentage tip would you like to give?\n")
 tipPercentage = int(tipPercentage)
 # tipPercentage = 12
 
-numPeople = input("How many people split the bill?")
+numPeople = input("How many people split the bill?\n")
 numPeople = int(numPeople)
 # numPeople = 7
 
