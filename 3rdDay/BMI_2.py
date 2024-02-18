8cbaf1403e6d0465b741c62eb11ba24051e27ca8
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-02-18 14:41:58 +0100

    BMI calc 2

---
+++ 3rdDay/BMI_2.py
@@ -0,0 +1,16 @@
+height = abs(int(input("Height in cm\n")))
+weight = abs(int(input("Weight in kg\n")))
+BMI = weight / ((height / 100) ** 2)
+
+print(BMI)
+
+if BMI < 18.5:
+	print("You are underweight.")
+elif BMI < 25:
+	print("You are a normal weight.")
+elif BMI < 30:
+	print("You are slightly overweight.")
+elif BMI < 35:
+	print("You are overweight.")
+else:
+	print("You are clinically obese.")
