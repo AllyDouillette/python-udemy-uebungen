d8ff7660fcc60319ee76b76c1c0a255d42414e5a
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-03-09 12:02:24 +0100

    for loops: average height

---
+++ 5th day/average_height.py
@@ -0,0 +1,14 @@
+heights = [151, 145, 178]
+sum = 0
+count = 0
+for current_height in heights:
+  # keep track of how many heights we have seen
+  count = count + 1
+  # add it to the sum
+  sum = sum + current_height
+
+avg_height = int(sum / count)
+
+print(f"""total height = {sum}
+number of students = {count}
+average height = {avg_height}""")
