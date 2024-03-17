d304d2c4bb3ab453b0e2ad0a3d686a1885fdf4af
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-03-17 11:23:47 +0100

    greeting function

--- 8th day/greet.py
+++ 8th day/greet.py
@@ -1,15 +1,23 @@
-def greet():
-  print("Hello!")
-  print("World!")
-  print("uwu")
+# def greet():
+#   print("Hello!")
+#   print("World!")
+#   print("uwu")
 
-def greet_by_name(name = "homie"):
-  print(f"Hello {name}!")
-  if (name != "Robert" and name != "Andrea"):
-    print(f"How do you do, {name}?")
-  else:
-    print("uwu hallo Mausi!")
+# def greet_by_name(name = "homie"):
+#   print(f"Hello {name}!")
+#   if (name != "Robert" and name != "Andrea"):
+#     print(f"How do you do, {name}?")
+#   else:
+#     print("uwu hallo Mausi!")
 
-greet_by_name("Andrea")
-greet_by_name("Eli")
-greet_by_name()
+# greet_by_name("Andrea")
+# greet_by_name("Eli")
+# greet_by_name()
+
+def greet_with(location, name):
+  if name == "":
+    name = "Test"
+  print(f"Hi {name}!")
+  print(f"What is it like in {location}?")
+
+greet_with("lol","")
